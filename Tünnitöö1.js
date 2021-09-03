af90f74546893f0e623c5f28f5626f5cb51f868c
ZaneyH <90028324+ZaneyH@users.noreply.github.com>
2021-09-03 11:05:11 +0200

    Rahakott kalkulaator

---
+++ Tünnitöö1.js
@@ -0,0 +1,7 @@
+rahakott = Math.random()*56
+ tootehind = 34 
+if (rahakott <= tootehind ) 
+    console.log("Sul on piisavalt")
+else
+console.log("Sa oled vaene")
+console.log("Vabajääk- "+rahakott)
