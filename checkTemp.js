24aa99ba1e234490f3dfb50f3fb4f392ba215219
Andreas Henri <90028324+ZaneyH@users.noreply.github.com>
2021-09-24 10:52:24 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,44 +1,4 @@
-function summa () {
-       console.log(5+6)
-}
-
-
-let rahakott = 10
-let tootehind = 5
- if (rahakott >= tootehind)
-     rahakott = 30
-     rahakott =rahakott - tootehind
-     rahakott += 10
-     
-     
-     let summa = function () {
-           
-           }
-           
-           //primitive types:
-           // number string boolean object function undefined null
-           // user defined types:
-           //Math
-           
-           TYPEOF= nätab mis tüüpi infot on muutuja sees
-           
-           Let töötaja = {nimi: "Henri", palk:10 }
-           let töötaja2= {
-           nimi: "Andrus"
-           palk: 23
-           rahakott: 5
-           maksaPalka: function () {
-            rahakott += palk 
-            }
-           }
-           
-         console.log(töötaja2.nimi)
-         console.log(töötaja2.rahakott)
-         let muuda = "palk"
-         töötaja2.muuda += 5
-         töötaja2["palk"] += 5
-         
-         
+    
 /* 
 katjal on kolm temperatuurivahemikku 
 0-20 on liiga külm     tagastab  -1
